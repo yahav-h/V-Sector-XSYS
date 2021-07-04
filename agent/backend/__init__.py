96a7391b2caa60ccb06e7e65d869f975e98efb57
Yahav-N-Hoffman <yahavh@avanan.com>
2021-07-04 17:11:02 +0300

    separate db agent modules

---
+++ agent/backend/__init__.py
@@ -0,0 +1,52 @@
+from sqlalchemy.ext.declarative import declarative_base
+from os.path import join, dirname, abspath, exists
+import logging
+
+__AGENT_DIR = 'agent'
+
+get_agent_db_path = lambda: join(dirname(dirname(abspath(__file__))), __AGENT_DIR) \
+    if exists(join(dirname(dirname(abspath(__file__))), __AGENT_DIR)) \
+    else Exception(f'[Err] path {join(dirname(dirname(abspath(__file__))), __AGENT_DIR)} does not exists')
+
+Base = declarative_base()
+
+
+class BaseDB:
+    log = None
+    db_session = None
+    engine = None
+    app_name = None
+
+    def __init__(self):
+        self.Base = Base
+        self.log = logging.getLogger(self.app_name)
+
+    def init_db(self):
+        try:
+            self.Base.metadata.create_all(bind=self.engine)
+            self.log.debug(f'[+] database schema created')
+            return True
+        except Exception as e:
+            self.log.error(f'[Err] error occur while initializing database\n{e}')
+            return False
+
+    def add(self, obj):
+        try:
+            self.db_session.add(obj)
+            self.db_session.commit()
+            self.log.debug(f'[+] new object {obj} added to database')
+            return True
+        except Exception as e:
+            self.log.error(f'[Err] error occur while adding object to database\n{e}')
+            return False
+
+    def delete(self, obj):
+        try:
+            self.db_session.delete(obj)
+            self.db_session.commit()
+            self.log.debug(f'[+] object {obj} deleted from database')
+            return True
+        except Exception as e:
+            self.log.error(f'[Err] error occur while deleting object from ddatabase\n{e}')
+            return False
+
